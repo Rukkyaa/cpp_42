252cdda1f4f34dd560a3c2702574e7dcc440ea17
Axel LAMBERT <lbrt.axel@gmail.com>
2023-02-24 18:54:01 +0100

    Exit when 0 zombie is input

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -6,14 +6,14 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 15:55:32 by axlamber          #+#    #+#             */
-/*   Updated: 2023/02/24 18:13:56 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/02/24 18:53:30 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie	*zombieHorde(int N, string name) {
-	if (N < 0)
+	if (N <= 0)
 	{
 		std::cout << "\033[1;31mError:" << "\033[0;30m the number of zombie in the horde can't be negative !\033[0m";
 		exit(0);
