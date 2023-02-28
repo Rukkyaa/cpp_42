40ad4dba1e2d447bec45b167f25b667c6907f3d0
Axel LAMBERT <lbrt.axel@gmail.com>
2023-02-28 10:24:12 +0100

    Add ifndef and define to the header + add underscore before private function

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 18:20:07 by axlamber          #+#    #+#             */
-/*   Updated: 2023/02/27 18:54:39 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/02/28 10:22:56 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,24 @@ Harl::~Harl() {
 	std::cout << "\033[1;31mHarl destroyed with success !\033[0m" << std::endl; 
 }
 
-void	Harl::debug(void) {
+void	Harl::_debug(void) {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do !" << std::endl;
 }
 
-void	Harl::info(void) {
+void	Harl::_info(void) {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
-void	Harl::warning(void) {
+void	Harl::_warning(void) {
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void	Harl::error(void) {
+void	Harl::_error(void) {
 	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
 
 void	Harl::complain(std::string level) {
-	void	(Harl::*function_ptr[4])(void)	= {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	void	(Harl::*function_ptr[4])(void)	= {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
 	std::string					tab_lvl[4]	= {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++)
