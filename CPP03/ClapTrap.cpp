05cf5ddf2c5902f5be11a46f76e5cb71945b0e6e
Axel LAMBERT <lbrt.axel@gmail.com>
2023-03-13 19:20:09 +0100

    Ex00 is done I guess

--- CPP03/ClapTrap.cpp
+++ CPP03/ClapTrap.cpp
@@ -6,7 +6,65 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 18:36:15 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/13 18:36:16 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/03/13 19:19:47 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( void ) :
+	_name("Nameless"),
+	_hitPoint(10),
+	_energyPoint(10),
+	_attackDamage(0){
+		std::cout << BOLD_GREEN"Clap trap " GREEN << _name << " created !" << std::endl;
+}
+
+ClapTrap::ClapTrap( std::string name ) :
+	_name(name),
+	_hitPoint(10),
+	_energyPoint(10),
+	_attackDamage(0),
+	_maxHitPoint(10){
+		std::cout << BOLD_GREEN"Clap trap " GREEN << _name << " created !" << std::endl;
+}
+
+ClapTrap::~ClapTrap( void ) {
+	(void)_hitPoint;
+	(void)_energyPoint;
+	(void)_attackDamage;
+	std::cout << BOLD_RED"Clap trap " RED << _name << " destroyed !" << std::endl;
+}
+
+void	ClapTrap::attack( const std::string &target) {
+	if (_energyPoint) {
+		std::cout << BOLD_CYAN"Attacked " << target << CYAN" and dealt " << _attackDamage << " damages !" << std::endl;
+		_energyPoint -= 1;
+	} else
+		std::cout << BOLD_RED"ERROR " RED << _name << " doesn't have enough energy points to attack ! " << std::endl;
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ) {
+	if (_hitPoint > 0) {
+		std::cout << BOLD_YELLOW"OOUCH ! " YELLOW << _name << " took " << amount << " damages " << std::endl;
+		if (_hitPoint - amount < 0)
+			_hitPoint = 0;
+		else
+			_hitPoint -= amount;
+	} else {
+		std::cout << BOLD_RED"WTF ? " RED << " do you really want to kick a dead ?!" << std::endl;
+	}
+}
+
+void	ClapTrap::beRepaired( unsigned int amount) {
+	if (_energyPoint) {
+		if (_hitPoint + amount < _maxHitPoint) {
+			std::cout << BOLD_MAGENTA"Wow ! " MAGENTA << _name << " regains life points !" << std::endl;
+			_energyPoint -= 1;
+			_hitPoint += amount;
+		} else {
+			std::cout << BOLD_RED"ERROR " RED << _name << " is at the max of his capacity !" << std::endl;
+		}
+	} else
+		std::cout << BOLD_RED"ERROR " RED << _name << " doesn't have enough energy points to attack ! " << std::endl;
+}
--- CPP03/main.cpp
+++ CPP03/main.cpp
@@ -6,7 +6,31 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 18:36:00 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/13 18:36:00 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/03/13 19:16:57 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int	main(void) {
+	ClapTrap	axel("Axel");
+
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.takeDamage(5);
+	axel.takeDamage(6);
+	axel.takeDamage(6);
+	axel.beRepaired(10);
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+	axel.attack("Alan");
+}
