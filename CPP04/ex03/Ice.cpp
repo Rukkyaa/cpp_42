be5440ef423bc4b5b8191f2337bb515b70f67b13
Axel LAMBERT <lbrt.axel@gmail.com>
2023-03-17 12:38:39 +0100

    Ice done

--- CPP04/ex03/Ice.cpp
+++ CPP04/ex03/Ice.cpp
@@ -6,13 +6,88 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:30:03 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/17 12:30:51 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/03/17 12:38:06 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ice.hpp"
 
+/*
+** Ice's default constructor, set type to "ice".
+** a message to say that the Ice is created is displayed if DEBUG is defined.
+**
+** @param void
+** @return void
+*/
 Ice::Ice( void ) : AMateria("ice") {
 	#ifdef DEBUG
-		cout << BOLD_GREEN << "Ice default constructor called" << RESET << endl;
+		cout << BOLD_GREEN << "[" << typeid(*this).name() + 1 << "] " RESET UNDERLINE_WHITE;
+		cout <<  _type << GREEN" created !" << RESET << endl;
+	#endif
+}
+
+/*
+** Ice's copy constructor, copy the given Ice.
+** a message to say that the Ice is created is displayed if DEBUG is defined.
+**
+** @param Ice const &rhs
+** @return void
+*/
+Ice::Ice( Ice const &rhs ) : AMateria(rhs) {
+	*this = rhs;
+	#ifdef DEBUG
+		cout << BOLD_GREEN << "[" << typeid(*this).name() + 1 << "] " RESET UNDERLINE_WHITE;
+		cout <<  _type << GREEN" created by copy !" << RESET << endl;
+	#endif
+}
+
+/*
+** Ice's operator= overload, copy the given Ice.
+** a message to say that the Ice is created is displayed if DEBUG is defined.
+**
+** @param Ice const &rhs
+** @return Ice
+*/
+Ice	&Ice::operator=( Ice const &rhs ) {
+	if (this != &rhs)
+		_type = rhs._type;
+	#ifdef DEBUG
+		cout << BOLD_GREEN << "[" << typeid(*this).name() + 1 << "] " RESET UNDERLINE_WHITE;
+		cout <<  _type << GREEN" assignation operator called !" << RESET << endl;
+	#endif
+	return (*this);
+}
+
+/*
+** Ice's default destructor, set type to "ice".
+** a message to say that the Ice is destroyed is displayed if DEBUG is defined.
+**
+** @param void
+** @return void
+*/
+Ice::~Ice( void ) {
+	#ifdef DEBUG
+		cout << BOLD_RED << "[" << typeid(*this).name() + 1 << "] " RESET UNDERLINE_WHITE;
+		cout <<  _type << RED" destroyed !" << RESET << endl;
+	#endif
+}
+
+/*
+** Ice's clone method, create a new Ice.
+**
+** @param void
+** @return AMateria*
+*/
+AMateria	*Ice::clone( void ) const {
+	return (new Ice(*this));
+}
+
+/*
+** Ice's use method, display a message.
+**
+** @param ICharacter const &target
+** @return void
+*/
+void		Ice::use( ICharacter &target ) {
+	cout << BOLD_BLUE << "* shoots an ice bolt at " << target.getName() << " *" << RESET << endl;
 }
--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -6,15 +6,16 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 18:31:11 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/17 12:24:48 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/03/17 12:36:46 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
+#include "Ice.hpp"
 
 int	main( void ) {
 	Character* zaz = new Character("zaz");
-	AMateria	test("Wood");
+	Ice			test;
 	
 	zaz->equip(&test);
 	Character* bob = new Character(*zaz);
