96978004f656271f4669a121320c264695250eca
Axel LAMBERT <lbrt.axel@gmail.com>
2023-04-12 10:24:38 +0200

    Added condition if the point is in the border of the triangle

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,20 +6,31 @@
 /*   By: axlamber <axlamber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:33:56 by axlamber          #+#    #+#             */
-/*   Updated: 2023/03/13 16:48:19 by axlamber         ###   ########.fr       */
+/*   Updated: 2023/04/12 10:22:12 by axlamber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Fixed	sign(Point const a, Point const b, Point const c) {
-	return ((a.getX() - c.getX()) * (b.getY() - c.getY()) - (b.getX() - c.getX()) * (a.getY() - c.getY()));
+static Fixed	ft_abs(Fixed x) {
+    if (x < 0)
+        return x * -1;
+    return x;
 }
 
-bool	bsp( Point const a, Point const b, Point const c, Point const point) {
-	bool	sign_a = sign(point, a, b) > 0;
-	bool	sign_b = sign(point, b, c) > 0;
-	bool	sign_c = sign(point, c, a) > 0;
-	
-	return (sign_a == sign_b && sign_b == sign_c);
+static Fixed	area( Point const a, Point const b, Point const c ) {
+    return ( ( ( a.getX() * ( b.getY() - c.getY() ) ) +
+             ( b.getX() * ( c.getY() - a.getY() ) ) +
+             ( c.getX() * ( a.getY() - b.getY() ) ) ) / 2 );
+}
+
+bool bsp( Point const a, Point const b, Point const c, Point const point) {
+    Fixed	abc = ft_abs(area( a, b, c ));
+    Fixed	pab = ft_abs(area( point, a, b ));
+    Fixed	pbc = ft_abs(area( point, b, c ));
+    Fixed	pca = ft_abs(area( point, c, a ));
+
+	if (pab == 0 || pbc == 0 || pca == 0)
+		return (false);
+    return  ( abc == pab + pbc + pca );
 }
