846f91632b494fceb46210806e35796031468584
Rukkyaa <lbrt.axel@gmail.com>
2023-04-15 22:43:35 +0200

    Makefile added

---
+++ CPP06/ex00/convert.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 22:38:47 by rukkyaa           #+#    #+#             */
+/*   Updated: 2023/04/15 22:43:00 by rukkyaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERT_HPP
+# define CONVERT_HPP
+
+# include <iostream>
+# include <cstdlib>
+
+# include "colors.hpp"
+
+using std::cout;
+using std::endl;
+
+#endif
---
+++ CPP06/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/15 22:38:22 by rukkyaa           #+#    #+#             */
+/*   Updated: 2023/04/15 22:42:42 by rukkyaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "convert.hpp"
+
+int main(int argc, char **argv)
+{
+	(void)(argv);
+	if (argc != 2)
+	{
+		cout << BOLD_RED "[ERROR] " RESET RED "Invalid number of arguments" << endl;
+		cout << "Usage: ./convert [string]" RESET << endl;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
