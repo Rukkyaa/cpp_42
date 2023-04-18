dcb00ed8ff5de4cd9322c37f4526b4ea3ae4a0da
Rukkyaa <lbrt.axel@gmail.com>
2023-04-18 19:54:39 +0200

    Add colors in the main, implement min/max, and test with int/float/string

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,24 +6,40 @@
 /*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 19:30:08 by rukkyaa           #+#    #+#             */
-/*   Updated: 2023/04/18 19:30:51 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2023/04/18 19:53:56 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 int main( void ) {
+	std::cout << "\033[3;36mGenerating 2 ints (a = 2, b = 3)..." << std::endl;
 	int a = 2;
 	int b = 3;
+	std::cout << "\033[3;33mSwapping a and b..." << std::endl;
 	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::cout << "\033[0;32ma = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cin.get();
+
+	std::cout << "\033[3;36mGenerating 2 floats (c = 2.1, d = 3.4)..." << std::endl;
+	float c = 2.1f;
+	float d = 3.4f;
+	std::cout << "\033[3;33mSwapping c and d..." << std::endl;
 	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "\033[0;32mc = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cin.get();
+
+	std::cout << "\033[3;36mGenerating 2 strings (e = chaine1, f = chaine2)..." << std::endl;
+	std::string e = "chaine1";
+	std::string f = "chaine2";
+	std::cout << "\033[3;33mSwapping c and d..." << std::endl;
+	::swap(e, f);
+	std::cout << "\033[0;32me = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
 	return 0;
 }
--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: rukkyaa <rukkyaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 19:26:50 by rukkyaa           #+#    #+#             */
-/*   Updated: 2023/04/18 19:35:12 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2023/04/18 19:45:41 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,14 @@ void	swap(T &a, T &b) {
 	tmp = b;
 	b = a;
 	a = tmp;
+}
+
+template<typename T>
+T	&min(T a, T b) {
+	return (a < b ? a : b);
+}
+
+template<typename T>
+T	&max(T a, T b) {
+	return (a > b ? a : b);
 }
